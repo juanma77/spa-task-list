14ec8871dffcdf21e09f6ea5544c85e8612beccc
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-05-27 01:32:36 -0500

    using map function to iterate the task array to show task fields

--- src/App.js
+++ src/App.js
@@ -4,9 +4,15 @@ import './App.css';
 import tasks from './data/tasks.json';
 
 class App extends Component {
+
+  state = {
+    tasks: tasks
+  }
+
   render(){
     return <div>
-      
+      {/* Aquí mostramos el "title" de cada task; usamos la función map para iterar el arreglo; esta función nos regresa un arreglo nuevo, nunca el mismo que iteramos. Por cada elemento que generamos cuando recorremos elementos de un array el debemos de ponerle una propiedad "key" */}
+      { this.state.tasks.map(e => <p key={e.title}>{ e.title } - { e.description } - { e.done }  { e.id } </p>)}
 
     </div>  
   }
