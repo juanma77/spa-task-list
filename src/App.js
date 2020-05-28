3ae33d925fc91ab9e895496650798b94f8a64ec9
Juan Manuel Lopez Santana <freakshield777@hotmail.com>
2020-05-28 13:09:31 -0500

    on change and on submit method added to task form component

--- src/App.js
+++ src/App.js
@@ -4,6 +4,8 @@ import './App.css';
 import tasks from './data/tasks.json';
 import TasksList from './components/TasksList';
 
+import TaskForm from './components/TaskForm';
+
 class App extends Component {
 
   state = {
@@ -13,6 +15,7 @@ class App extends Component {
   render(){
     return <div>
       {/* Aquí mandamos la propiedad que es un arreglo llamado "tasks", que puede ser cualquier otro nombre, desde este componente que es el padre, al componente TasksList, que sería el hijo; en el componente TasksList ahora debemos recibir esta propiedad con "this.props.tasks"  */}
+      <TaskForm/>
       <TasksList tasks={ this.state.tasks } />
     </div>  
   }
--- src/components/TaskForm.js
+++ src/components/TaskForm.js
@@ -2,4 +2,36 @@ import React, { Component } from 'react';
 
 export default class TaskForm extends Component {
 
+    state = {
+        title: '',
+        description: ''
+    }
+
+    // Esta función es para prevenir que se envíen los datos del formulario cuando damos clic en el botón de Enviar 
+    onSubmit = event => {
+        console.log("Sending...", event);
+        event.preventDefault();
+       
+    }
+
+    // El event.target.value sirve para estar escuchando y que se muestre en consola cada vez que se escribe dentro de los input
+    onChange = event => {
+        //console.log(event.target.value);
+        this.setState({
+            [event.target.name] :  event.target.value
+        })
+        console.log(event.target.name, event.target.value);
+    }
+
+    render(){
+        return (
+            <form onSubmit={ this.onSubmit }>
+                <input className="form-inline" type="text" placeholder="Type a Task" onChange={this.onChange} value={this.state.title} name="title"/>
+                <br/>
+                <textarea className="form-group" placeholder="Type a Description" onChange={this.onChange} value={this.state.description} name="description">
+                </textarea> <br/>
+                <button className="btn btn-primary" type="submit">Create</button>
+            </form>
+        )
+    }
 }
